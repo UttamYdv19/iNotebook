237aeec1bba98a65e2a376b1f6e16741ccc535d6
UttamYdv19 <uttamyadav1912@gmail.com>
2025-07-13 00:36:54 +0530

    fix corse issue

--- Backend/app.js
+++ Backend/app.js
@@ -5,22 +5,32 @@ const cors = require("cors");
 
 const app = express();
 const PORT = process.env.PORT || 8000;
+const allowedOrigin = [
+  "http://localhost:3000",
+  "https://i-notebook-nine-eta.vercel.app",
+];
 
 // Connect to MongoDB
 connectToMongo();
 
 // Middlewares
-app.use(cors());
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigin.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("cors not allowed"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/note"));
-
-// Default route (optional)
-app.get("/", (req, res) => {
-  res.send("Welcome to the iNotebook backend API 🎉");
-});
 
 // Start server
 app.listen(PORT, () => {
